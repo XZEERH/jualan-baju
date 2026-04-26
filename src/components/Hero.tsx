a7a01aea3cb51e5ea66f59c8adb76a85a54549ba
XZEERH <razzyr408@gmail.com>
2026-04-26 11:04:54 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="relative h-screen w-full flex items-center justify-center bg-white px-6">
       <div className="text-center fade-in">
-        <span className="font-signature text-3xl md:text-4xl text-gray-700 block mb-2">Rama Shop</span>
+        <span className="font-signature text-3xl md:text-4xl text-gray-700 block mb-2">Razeerh Shop</span>
         <h1 className="text-6xl md:text-9xl font-bold tracking-tighter mb-10 leading-[0.85]">
           STAY <br /> <span className="font-signature font-normal text-5xl md:text-8xl tracking-normal lowercase">classy.</span>
         </h1>
