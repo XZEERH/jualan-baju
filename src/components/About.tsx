c3130499b88b81a69d332ecdfc9b88d7d77aba6e
XZEERH <razzyr408@gmail.com>
2026-04-26 11:04:02 +0700

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,13 +5,13 @@ const About = () => {
         <h2 className="text-[10px] font-bold uppercase tracking-[0.5em] mb-12 text-gray-300 italic underline underline-offset-8">Our Story</h2>
         <div className="space-y-10 text-black leading-snug">
           <p className="font-signature text-2xl md:text-4xl fade-in">
-            Selamat datang di Rama Shop. Kami percaya bahwa setiap orang berhak tampil elegan tanpa harus berkompromi dengan kenyamanan. Koleksi kami dirancang dengan filosofi minimalisme yang tajam, memastikan setiap helai pakaian memberikan kesan eksklusif dan profesional.
+            Selamat datang di Razeerh Shop. Kami percaya bahwa setiap orang berhak tampil elegan tanpa harus berkompromi dengan kenyamanan. Koleksi kami dirancang dengan filosofi minimalisme yang tajam, memastikan setiap helai pakaian memberikan kesan eksklusif dan profesional.
           </p>
           <p className="font-signature text-2xl md:text-4xl fade-in">
-            Kualitas adalah inti dari setiap langkah kami. Dengan memilih material terbaik dan proses penjahitan yang teliti, Rama Shop menawarkan produk tahan lama dengan harga yang tetap terjangkau. Kami berkomitmen menyediakan fashion yang tidak hanya mengikuti tren, tetapi menjadi investasi gaya Anda dalam jangka panjang.
+            Kualitas adalah inti dari setiap langkah kami. Dengan memilih material terbaik dan proses penjahitan yang teliti, Razeerh Shop menawarkan produk tahan lama dengan harga yang tetap terjangkau. Kami berkomitmen menyediakan fashion yang tidak hanya mengikuti tren, tetapi menjadi investasi gaya Anda dalam jangka panjang.
           </p>
           <p className="font-signature text-2xl md:text-4xl fade-in">
-            Kepuasan Anda merupakan prioritas mutlak di Rama Shop. Kami memastikan setiap transaksi berjalan mulus dengan standar kontrol kualitas yang ketat. Bergabunglah dengan ribuan pelanggan yang telah merasakan kenyamanan maksimal dan tingkat kepercayaan diri yang baru bersama koleksi esensial kami.
+            Kepuasan Anda merupakan prioritas mutlak di Razeerh Shop. Kami memastikan setiap transaksi berjalan mulus dengan standar kontrol kualitas yang ketat. Bergabunglah dengan ribuan pelanggan yang telah merasakan kenyamanan maksimal dan tingkat kepercayaan diri yang baru bersama koleksi esensial kami.
           </p>
         </div>
       </div>
